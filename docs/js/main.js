868f03862fc5eaf88fe21115688a4214a80236c3
FoxWeb <dneprfox@gmail.com>
2021-06-06 18:13:34 +0300

    git

---
+++ docs/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+	$(".mobile-menu,.services__btn").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1200);
+    
+});
+
+})
