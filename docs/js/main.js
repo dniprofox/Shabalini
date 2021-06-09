de73557c754ae60be4b6815d0797d4d80d88b3a0
FoxWeb <dneprfox@gmail.com>
2021-06-09 15:45:06 +0300

    changes

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,20 +1,35 @@
 $(function () {
-	$(".menu__nav,.mobile-menu__nav,.services__btn").on("click","a", function (event) {
-    event.preventDefault();
-    var id  = $(this).attr('href'),
-        top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 1200);    
-    });
-    $(".mobile-menu__nav,.services__btn").on("click","a", function (event) {
+    $(".menu__nav,.mobile-menu__nav,.services__btn").on("click", "a", function (event) {
         event.preventDefault();
-        var id  = $(this).attr('href'),
+        var id = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top-100}, 1200);    
+        $('body,html').animate({ scrollTop: top }, 1200);
     });
-        //(xxx) xxx-xxxx format code
-        $('#user_phone').usPhoneFormat({
-            format: '(xxx) xxx-xxxx',
-        });   
+    $(".mobile-menu__nav,.services__btn").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top - 100 }, 1200);
+    });
     
 
-})
+    /* Кнопка вверх */
+   $(window).on('scroll',function () {
+      
+    if ($(this).scrollTop() != 0)
+       $('#toTop').fadeIn("slow");
+    else
+       $('#toTop').fadeOut("slow");
+ });
+ $('#toTop').on('click',function () {
+    $('body,html').animate({
+       scrollTop: 0
+    }, 800);
+ });
+    
+    
+    
+});
+    
+    
+
