86f1b0671f256f13896f409fa8a28d4a19364679
FoxWeb <dneprfox@gmail.com>
2021-06-10 22:29:13 +0300

    changes

--- docs/js/main.js
+++ docs/js/main.js
@@ -27,29 +27,57 @@ $(function () {
         $('body,html').animate({
         scrollTop: 0
         }, 800);
-    });
+    });    
+
+    // *****mail******
+    // $('form').on('submit',function(e) {
+    //     e.preventDefault();
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "mailer/smart.php",
+    //         data: $(this).serialize()
+    //     }).done(function() {
+    //         $(this).find("input").val("");
+    //         $('#modal__form').fadeOut();
+    //         $('.overlay, #thanks').fadeIn('slow');
+
+    //         $('form').trigger('reset');
+    //     });
+    //     return false;
+    // });  
     
-    //E-mail Ajax Send
-	$("form").on( "submit",function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-
-
-
+    //  mask
+    $('input[name=user_phone]').mask("+38 (999) 999-99-99");
     
+    // form_validate
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                user_name: {
+                    required: true,
+                    minlength: 2
+                },
+                user_phone: "required"                
+            },
+            messages: {
+                user_name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                user_phone: "Пожалуйста, введите свой номер телефона",
+                user_text: {
+                    required: "Пожалуйста, введите сообщение",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                               
+            }
+        });
+    };
+    validateForms('#modal__form');
+    
+
+
+
     
 });
     
